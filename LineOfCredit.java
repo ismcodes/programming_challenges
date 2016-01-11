4bc326ff0a8e4cb1c1001b559e2c3e5e10dc5400
Isaac Moldofsky <isaac.moldofsky@gmail.com>
2016-01-11 00:42:45 -0600

    add more comments/explanation

--- LineOfCredit.java
+++ LineOfCredit.java
@@ -10,41 +10,44 @@ class LineOfCredit {
   // is the credit limit different then loan amount? I suppose it could be, but that does
   // not happen in the examples
   private static double netAmountLoaned, totalInterestDue = 0.0;
-  private static int lastTransactionDay = 1;
+  private static int lastTransactionDay = 0; // to help calculate interest
   public static void main(String[] args){
-    if(args.length == 0){
+    if(args.length != 3){
+      // this is run from the command line using arguments
       System.out.println("usage: java LineOfCredit 35 1000 eventlist1.csv");
-      System.exit(0);
+      // print out example usage if user is not sure how to use it
+      System.exit(0); // have to exit because the arguments are likely not valid 
     }
     APR = Double.parseDouble(args[0])/100;
     CREDIT_LIMIT = Double.parseDouble(args[1]);
     Path eventListFile = Paths.get(args[2]);
-    try {
+    try{
       Stream<String[]> events = Files.lines(eventListFile).map(line -> line.split(","));
       events.forEach(eventArray -> {
         // format: [day, pay/draw, amount]
         int today = Integer.parseInt(eventArray[0]);
-        double interestFromLastInterval = netAmountLoaned * (today - lastTransactionDay) / 365 * APR;
+        double interestFromLastInterval = netAmountLoaned * (today - lastTransactionDay) / 365 * APR; // interest formula given in the instructions
         double amount = Double.parseDouble(eventArray[2]);
-        if (eventArray[1].equals("pay")) {
+        if(eventArray[1].equals("pay")){
           netAmountLoaned -= amount;
           totalInterestDue += interestFromLastInterval;
           lastTransactionDay = today;
         }
-        else if (netAmountLoaned + amount < CREDIT_LIMIT) {
+        else if(netAmountLoaned + amount < CREDIT_LIMIT){
           netAmountLoaned += amount;
           totalInterestDue += interestFromLastInterval;
           lastTransactionDay = today;
           // I repeated these two lines because I thought it would be more readable/less clunky then creating another variable
           // `isValidLoan` or something. I originally wanted to have `continue` if the loan was not valid, and then if the end of
           // the iteration was reached the interest due and last transaction day would be updated.
+          // however this is not possible to do with streams. Not using streams could be an option.
         }
       });
       events.close(); // close stream
       //last interest payment
       totalInterestDue += netAmountLoaned * (30 - lastTransactionDay) / 365 * APR;
       System.out.println("You still owe: " + netAmountLoaned + " and " + totalInterestDue + " for interest");
-      System.out.println("Total owed: " + (netAmountLoaned + totalInterestDue));
+      System.out.println("Total payment: " + (netAmountLoaned + totalInterestDue));
     }
     catch (IOException e){
       System.out.println("Error reading file: " + e.getMessage());
--- tweetstream.rb
+++ tweetstream.rb
@@ -1,22 +1,33 @@
 require 'tweetstream'
 require 'redis'
-redis = Redis.new
-starting_time = Time.now
+
+redis = Redis.new # first time using redis, thought would be useful because it's a k-v store
 TweetStream.configure do |config|
+  # don't include keys and stuff in here cause putting on github
 end
 STOP_WORDS = ["https"]
+# only a few "stop words" were given in the instructions, so I kept running this
+# to find more stop words to include in here. Eventually I realized that pretty much
+# all the words that were <= 4 characters were stuff like 'with', 'z', 'to', etc.
+# which I interpreted as stop words. So I just filtered by word length and didn't include "https" either.
+starting_time = Time.now
 TweetStream::Client.new.sample do |status|
-  break if Time.now - starting_time > 60 
-  words = status.text.scan(/\w+/)
+  break if Time.now - starting_time > 300
+  # every iteration, check the time to make sure it has been streaming for < 5 minutes
+  words = status.text.scan(/\w+/) # get every word from the tweet
   words.each do |word|
     unless STOP_WORDS.include? word.downcase or word.length < 5
-      redis.setnx(word, 0)
-      redis.incr(word)
+      redis.setnx(word, 0) # if this word does not have a frequency in redis, create it = 0
+      redis.incr(word) # add one to the frequency count in redis
     end
   end
 end
-top_10 = redis.keys.map{|word| [word, redis.get(word)]}.sort{|w1,w2| w1[1].to_i <=> w2[1].to_i}.reverse[0..10]
-# don't include time elapsed as one of the words
+top_10 = redis.keys # nothing stored in this instance besides words, so just get all keys
+              .map{|word| [word, redis.get(word)]} # grab the frequency of each word as well
+              .sort{|w1,w2| w1[1].to_i <=> w2[1].to_i} # sort by frequency (2nd item in subarray)
+              .reverse # order descending
+              .first(10) # take first 10 from sorted data
+
 puts Hash[top_10]
 # program has finished, so redis can be reset now
 redis.flushDB
